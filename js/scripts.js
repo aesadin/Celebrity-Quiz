80b18f2fbc6644ae7f9786eb634c73803f702644
Allison Sadin <aesadin@gmail.com>
2020-06-04 11:46:04 -0700

    create two more radio buttons and jacascript code for them
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+  const date = $("input:radio[name=date]:checked").val();
+  const food = $("input:radio[name=food]:checked").val();
+  })
+}
